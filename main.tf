8655e2e826da24a2110fe511708d070f2244a03a
ptonini <pedro.tonini@gmail.com>
2024-12-31 21:18:40 -0300

    chore: first release

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "vault_auth_backend" "this" {
+  type = var.type
+  path = var.path
+}
+
+resource "vault_kubernetes_auth_backend_config" "this" {
+  backend                = vault_auth_backend.this.path
+  kubernetes_host        = var.kubernetes_host
+  kubernetes_ca_cert     = var.kubernetes_ca_cert
+  token_reviewer_jwt     = var.token_reviewer_jwt
+  pem_keys               = var.pem_keys
+  disable_iss_validation = var.disable_iss_validation
+}
+
+module "roles" {
+  source                           = "app.terraform.io/ptonini-org/kubernetes-auth-role/vault"
+  version                          = "~> 1.0.0"
+  for_each                         = var.roles
+  name                             = each.key
+  backend                          = vault_auth_backend.this.path
+  bound_service_account_names      = coalesce(each.value.bound_service_account_names, [each.key])
+  bound_service_account_namespaces = each.value.bound_service_account_namespaces
+  policy_definitions               = each.value.policy_definitions
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "type" {
+  default = "kubernetes"
+}
+
+variable "path" {
+  default = "kubernetes"
+}
+
+variable "kubernetes_host" {}
+
+variable "kubernetes_ca_cert" {}
+
+variable "token_reviewer_jwt" {}
+
+variable "pem_keys" {
+  type    = set(string)
+  default = []
+}
+
+variable "disable_iss_validation" {
+  default = true
+}
+
+variable "roles" {
+  type = map(object({
+    name                             = optional(string)
+    audience                         = optional(string)
+    token_ttl                        = optional(number)
+    token_max_ttl                    = optional(number)
+    bound_service_account_names      = optional(set(string))
+    bound_service_account_namespaces = optional(set(string))
+    policies                         = optional(list(string))
+    policy_definitions               = optional(list(string))
+  }))
+  default = {}
+  nullable = false
+}
